378a7875f458d6e12cced3e4e57e2bbe051ba24c
Karola <yumi.chan@interia.pl>
2016-08-26 14:38:21 +0200

    wyjatki

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hejka Świecie!");
+
+
+        try {
+            String a = null;
+            System.out.println("Hejka Świecie!");
+        } catch (NullPointerException e) {
+            System.out.println("Żegnaj Świecie!");
+        }
+
+
+    }
+
+    public void Dog() {
+        try {
+            int cat[] = new int[12];
+            cat[16] = 12;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("whaju?");
+        }
     }
 }
