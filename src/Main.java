1e7f95b48a9cb90df6f8d3ec13b6a907802c4524
widzew <klop99@wp.pl>
2016-08-26 14:33:03 +0200

    petle

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hejka Świecie!");
+
+        int liczba = 1;
+
+        while (liczba < 4) {
+            System.out.println("Jazda z koksem");
+            liczba++;
+            System.out.println("Koniec");
+        }
+
+        for (liczba = 1; liczba < 4; liczba++) {
+            System.out.println("Walimy");
+        }
+        System.out.println("Już nie");
+
+        do System.out.println("Drukujemy");
+        while (liczba > 4);
+
+        ArrayList lista = new ArrayList();
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+
+        for (Object listaliczb : lista) {
+            System.out.println("Liczba " + listaliczb);
+        }
+
     }
+
 }
+
+
